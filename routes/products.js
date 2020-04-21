6fe61870d00185ff758a456ba8ce9a516885a2a4
Elvinas Lukasevicius <elvis@Elvinass-MBP.lan>
2020-04-21 22:02:45 +0300

    Products code cleanup

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const products = [1, 2, 3, 4, 5];
 
-/* Get all products. */
 router.get('/', function(req, res) {
-  console.log('id: ')
   res.send(products);
 });
 
